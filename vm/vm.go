183b151b86fe2b0c8f38c48e72a55ea4ae6d2ee2
vilkris <vilkris@protonmail.com>
2024-06-03 22:19:13 +0300

    Revert changes to AddAccountBlockTransaction for now

--- vm/vm.go
+++ vm/vm.go
@@ -294,9 +294,8 @@ func NewMomentumVM(context vm_context.MomentumVMContext) *MomentumVM {
 func (vm *MomentumVM) applyMomentum(pool chain.AccountPool, momentum *nom.Momentum) error {
 	momentumStore := vm.context
 
-	identifier := momentumStore.Identifier()
 	for _, header := range momentum.Content {
-		if err := momentumStore.AddAccountBlockTransaction(*header, pool.GetPatch(header.Address, header.Identifier()), identifier); err != nil {
+		if err := momentumStore.AddAccountBlockTransaction(*header, pool.GetPatch(header.Address, header.Identifier())); err != nil {
 			return err
 		}
 	}
